2494046ad364be8827e9734dee866def3d3c081b
Ben Babington <benb@fsbtech.com>
2019-01-16 18:14:53 +0000

    initial setup start

---
+++ pages/admin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class admin extends Component {
+    render() {
+        return (
+            <div>
+                hello this is admin page
+            </div>
+        );
+    }
+}
+
+export default admin;
